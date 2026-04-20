5e1836ea56843c01cddab809c1dab2fb9208c5b9
Anton Shcherbakov <1mailanton@gmail.com>
2026-04-20 17:47:04 +0200

    I forgot this files to minesweeper game

---
+++ web-minesweeper/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FieldCells from "./component/UI/fieldCells/fieldCells";
+
+function App() {
+  return (
+    <div className="App">
+      <FieldCells/>
+    </div>
+  );
+}
+
+export default App;
